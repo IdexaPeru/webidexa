1a9025b69f365264dc83bf940f9cf0df953390da
yelsino <yelsino321@gmail.com>
2021-12-09 18:40:46 -0500

    cambios 05

--- src/Variables/globales.js
+++ src/Variables/globales.js
@@ -1,2 +1,2 @@
-export const NUMERO_MOVIL = '993113084'
-export const MENSAJE_WHATSAPP = 'Me%20interesa%20los%20servicios%20que%20estoy%20vendiendo'
+export const NUMERO_MOVIL = '51993113084'
+export const MENSAJE_WHATSAPP = 'Me%20interesa%20los%20servicios%20que%20estoy%20viendo'
